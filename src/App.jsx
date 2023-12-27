22efc55785bd64fbd78ce4e1ddf0a4c3795bd5cd
Pedro Lobato <pedrolfirbida@gmail.com>
2023-12-27 12:29:43 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="Container">
+      <div className="Header">
+        <h1>Deixe sua avaliação</h1>
+        <h2>Sua avaliação nos ajuda a melhorar nossos serviços.</h2>
+      </div>
+      <div className="Form-Container">
+        <p>Etapas</p>
+      </div>
+      <div className="Actions">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
